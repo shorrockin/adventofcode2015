a805033218dabc3498031632e3aeee1d923b38d0
Chris Shorrock <cs@stripe.com>
2024-01-15 08:05:34 -0800

    day 01 cleanup

--- day-01/src/lib.rs
+++ day-01/src/lib.rs
@@ -1,23 +1,20 @@
 pub fn part_one(input: &str) -> i32 {
-    *floors(input).last().unwrap()
+    floors(input).last().unwrap()
 }
 
 pub fn part_two(input: &str) -> usize {
-    floors(input).iter().position(|floor| *floor == -1).unwrap() + 1
+    floors(input).position(|floor| floor == -1).unwrap() + 1
 }
 
-fn floors(input: &str) -> Vec<i32> {
-    input
-        .chars()
-        .scan(0, |acc, c| {
-            *acc += match c {
-                '(' => 1,
-                ')' => -1,
-                _ => panic!("unexpected character"),
-            };
-            Some(*acc)
-        })
-        .collect()
+fn floors(input: &str) -> impl Iterator<Item = i32> + '_ {
+    input.chars().scan(0, |acc, c| {
+        *acc += match c {
+            '(' => 1,
+            ')' => -1,
+            _ => panic!("unexpected character"),
+        };
+        Some(*acc)
+    })
 }
 
 #[cfg(test)]
@@ -27,12 +24,8 @@ mod tests {
     static INPUT: &str = include_str!("input.txt");
 
     #[test]
-    fn test_part_one() {
+    fn test_parts() {
         assert_eq!(74, part_one(INPUT));
-    }
-
-    #[test]
-    fn test_part_two() {
         assert_eq!(1795, part_two(INPUT));
     }
 }
